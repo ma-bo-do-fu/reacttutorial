a82fad75aea7f89ca9a9fee693c1d34fda6e2658
ma-bo-do-fu <sby02773@gmail.com>
2017-04-12 16:36:01 +0900

    add files

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+}
